436dbaf1860d6a625214e46f5c2be273158b695c
Betty Chow <bettychchow@gmail.com>
2018-05-26 15:21:51 -0700

    Add radio buttons and save button to main page

--- app/src/main/java/com/example/android/emocoach/MainActivity.java
+++ app/src/main/java/com/example/android/emocoach/MainActivity.java
@@ -2,23 +2,32 @@ package com.example.android.emocoach;
 
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
+import java.io.SyncFailedException;
 import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
 
     private TextView theDate;
     private TextView currentDate;
+    private TextView emotions;
     private Button btnGoCalendar;
+    private Button btnSave;
     private int cDay;
     private int cMonth;
     private int cYear;
+    private RadioGroup rGroup;
+    private RadioButton checkedRadioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
         theDate = (TextView) findViewById(R.id.date);
         currentDate = (TextView)  findViewById((R.id.today));
         btnGoCalendar = (Button) findViewById(R.id.btnGoCalendar);
+        btnSave = (Button) findViewById(R.id.btnSave);
 
 //        Intent incomingIntent = getIntent();
 //        String date = incomingIntent.getStringExtra("date");
@@ -38,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
         cMonth = calander.get(Calendar.MONTH) + 1;
         cYear = calander.get(Calendar.YEAR);
 
-        currentDate.setText("Today is " + " " + cMonth + " " + cDay + " " + cYear);
+        currentDate.setText("Today is " + "" + cMonth + "/" + cDay + "/" + cYear);
 
 
 
@@ -49,6 +59,49 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+
+        rGroup = (RadioGroup)findViewById(R.id.myRadioGroup);
+
+        emotions = (TextView)findViewById(R.id.emotions);
+        //checkedRadioButton = (RadioButton)rGroup.findViewById(rGroup.getCheckedRadioButtonId());
+
+//        btnSave.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                int selectedRadioButtonID = rGroup.getCheckedRadioButtonId();
+//
+//                if (selectedRadioButtonID != -1) {
+//
+//                    RadioButton selectedRadioButton = (RadioButton) findViewById(selectedRadioButtonID);
+//                    String selectedRadioButtonText = selectedRadioButton.getText().toString();
+//
+//                    emotions.setText(selectedRadioButtonText + " selected.");
+//                }
+//                else{
+//                    emotions.setText("Nothing selected from Radio Group.");
+//                }
+//
+//            }
+//        });
+
+
+        rGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton checkedRadioButton = (RadioButton)group.findViewById(checkedId);
+
+                String radioTextValue = checkedRadioButton.getText().toString();
+                Drawable radioImgValue = checkedRadioButton.getCompoundDrawables()[0];
+
+                emotions.setText(radioTextValue);
+                emotions.setCompoundDrawablesWithIntrinsicBounds (null, null, radioImgValue, null);
+            }
+        });
+
+
 
     }
 }
